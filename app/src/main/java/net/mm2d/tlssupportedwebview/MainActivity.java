670cfb2d708493dddb9949f506583e5444b3947e
OHMAE Ryosuke <ryo@mm2d.net>
2017-11-28 23:39:36 +0900

    add copyright

--- app/src/main/java/net/mm2d/tlssupportedwebview/MainActivity.java
+++ app/src/main/java/net/mm2d/tlssupportedwebview/MainActivity.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2017 大前良介 (OHMAE Ryosuke)
+ *
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/MIT
+ */
+
 package net.mm2d.tlssupportedwebview;
 
 import android.annotation.SuppressLint;
